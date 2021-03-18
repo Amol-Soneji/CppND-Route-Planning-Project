930d0da9e8ffa68c9aa47ca28455eb2a13738b37
Amol <amol@kali.linux-kali>
2021-03-18 06:10:22 -0400

    Completed the TODO's in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,15 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter the value for the starting x coordinate:  " << "\n";
+    std::cin >> start_x;
+    std::cout << "Enter the value for the starting y coordinate:  " << "\n";
+    std::cin >> start_y;
+    std::cout << "Enter the value for the ending x coordinate:  " << "\n";
+    std::cin >> end_x;
+    std::cout << "Enter the value for the ending y coordinate:  " << "\n";
+    std::cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
